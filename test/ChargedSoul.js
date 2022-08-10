217083cf3498c3c63a252a0ce1ff504f94ee418e
scammi <scammi@gmail.com>
2022-08-10 13:22:26 -0300

    Mint soul allow listed

--- test/ChargedSoul.js
+++ test/ChargedSoul.js
@@ -8,28 +8,34 @@ const chargedParticlesMainnetAddress = require("@charged-particles/protocol-subg
 const chargedSettingsMainnetAddress = chargedParticlesMainnetAddress.chargedSettings.address; 
 const chargedParticlesContractMainnetAddress = chargedParticlesMainnetAddress.chargedParticles.address; 
 
-let customNFTdeployedAddress, adminAddress, provider;
+let customNFTdeployedAddress, adminAddress, provider, customNFT, signers;
 
 describe("Charged Particles whitelist ", async() => {
-  provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL_MAINNET, 1);
   const ChargedParticlesContract = new ethers.Contract(chargedParticlesContractMainnetAddress, ChargedParticlesAbi);
   const ChargedSettingContract = new ethers.Contract(chargedSettingsMainnetAddress, ChargedSettingsAbi);
-
+  provider = new ethers.providers.StaticJsonRpcProvider(process.env.RPC_URL_MAINNET, 1);
+  
   beforeEach(async() => {
     // Deploy custom NFT
     const CustomNFT = await ethers.getContractFactory("Soul"); 
-    const customNFT = await CustomNFT.deploy();
+    customNFT = await CustomNFT.deploy();
     const customNFTdeployed = await customNFT.deployed();
-  
+    
     // Get Charged Particle owner address
     adminAddress = await ChargedParticlesContract.connect(provider).owner();
     customNFTdeployedAddress = customNFTdeployed.address;
-  
+    
     // impersonate admin account 
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [adminAddress],
     });
+    
+    const owner = await ethers.getSigner(adminAddress);
+    const whiteListTx = await ChargedSettingContract.connect(owner).enableNftContracts([customNFTdeployedAddress]);
+    await whiteListTx.wait();
+
+    signers = await ethers.getSigners();
   });
 
   afterEach(async () => {
@@ -44,14 +50,19 @@ describe("Charged Particles whitelist ", async() => {
     expect(contractResponse).to.be.equal(chargedParticlesMainnetAddress.chargedState.address)
   });
   
-  it ("Become admin and whitelist custom token.", async() => {
-    // Whitelist custom NFT
-    const owner = await ethers.getSigner(adminAddress);
-    const whiteListTx = await ChargedSettingContract.connect(owner).enableNftContracts([customNFTdeployedAddress]);
-    await whiteListTx.wait();
+  it ('Mints, locks and energize', async() => {
+    const minter = signers[1];
+    const soulNFT = await customNFT.connect(minter);
+
+    const mintLockTx = await soulNFT.lockMint(minter.address, 'www.test.com');
+    await mintLockTx.wait();
+
+    expect(await soulNFT.ownerOf(1)).to.equal(minter.address);
+    expect(await soulNFT.locked(1)).to.equal(true);
+    await expect(soulNFT.transferFrom(
+      minter.address,
+      signers[2].address,
+      1
+    )).to.revertedWith("Locked token");
   });
-
-  // it ('Mints, locks and energize', async() => {
-
-  // });
 });
